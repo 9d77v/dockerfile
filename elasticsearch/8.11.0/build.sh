9b7cb9bcc5315c773ef27b23831f8047f4fd29db
9d77v <9d77v@9d77v.me>
2023-11-30 15:36:38 +0800

    feat: add elasticsearch 8.11.0

---
+++ elasticsearch/8.11.0/build.sh
@@ -0,0 +1,11 @@
+echo "check if command 'sudo' exist"
+SUDO=sudo
+if command -v sudo >/dev/null 2>&1; then
+    echo 'sudo exist'
+else
+    echo 'sudo not exist'
+    SUDO=""
+fi
+
+$SUDO docker build -t 9d77v/elasticsearch:8.11.0 .
+$SUDO docker push 9d77v/elasticsearch:8.11.0
